72f965e2ba903fd2c72fc352acdcf09bd2c937c0
Ben Cordero <bmc@linux.com>
2012-07-07 06:34:41 +0100

    listener: provide the port on the command line

--- listener.py
+++ listener.py
@@ -4,9 +4,10 @@ log = logging.getLogger(__name__)
 
 import select
 import socket
+import sys
 
 open_sockets = []
-def listen(family=socket.AF_INET, port=settings.UDPGEN_LISTEN_PORT):
+def listen(family=socket.AF_INET, port=0):
     s = socket.socket(family, socket.SOCK_DGRAM)
     s.bind(("", port))
     log.info("Listening on %s:%d" % (s.getsockname()[0], s.getsockname()[1]))
@@ -19,10 +20,13 @@ def receive_data(addr, data):
         ''.join([hex(ord(c))[2:] for c in data])
     ))
 
-
 if __name__ == '__main__':
-    listen(socket.AF_INET)
-    listen(socket.AF_INET6)
+    try:
+        port = int(sys.argv[1])
+    except IndexError:
+        port = settings.UDPGEN_LISTEN_PORT
+    listen(socket.AF_INET, port)
+    listen(socket.AF_INET6, port)
 
     while True:
         r, w, x = select.select(open_sockets, [], [])
